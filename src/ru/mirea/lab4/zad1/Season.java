a465e58f99616050d2a79f64fae184b943ed1f45
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-09-28 20:12:55 +0300

    Initial commit

---
+++ src/ru/mirea/lab4/zad1/Season.java
@@ -0,0 +1,34 @@
+package ru.mirea.lab4.zad1;
+
+public enum Season {
+    SUMMER("hot and dry", 30){
+        public String getDescription(){
+            return "Теплое время года";
+        }
+    },
+    SPRING("slush and comfort", 15),
+    WINTER("it's cold and beautiful", -10){
+        public String getDescription(){
+            return "Холодное время года";
+        }
+    },
+    AUTUMN("Cloudy and atmospheric", 12);
+
+
+    Season(){
+    }
+    private String info;
+    private int temperature;
+    Season(String info, int temperature){
+        this.info = info;
+        this.temperature = temperature;
+    }
+    public String getInfo(){
+        return name() + "\t" +  info + "\t" + temperature;
+    }
+
+    String getDescription(){
+        return "";
+    }
+
+}
---
+++ src/ru/mirea/lab4/zad1/Tester.java
@@ -0,0 +1,31 @@
+package ru.mirea.lab4.zad1;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        Season season_favourite = Season.SUMMER;
+        System.out.println(season_favourite.getInfo() + "\n");
+        Scanner sourse = new Scanner(System.in);
+        Season s = Season.valueOf(sourse.next());
+        switch (s){
+            case WINTER:
+                System.out.println("The time of the year is winter\n");
+                break;
+            case SPRING:
+                System.out.println("The time of the year is spring\n");
+                break;
+            case SUMMER:
+                System.out.println("The time of the year is summer\n");
+                break;
+            case AUTUMN:
+                System.out.println("The time of the year is autumn\n");
+                break;
+        }
+        System.out.println(season_favourite.getDescription() + "\n");
+        Season[] season = Season.values();
+        for(Season x: season){
+            System.out.println(x.getInfo());
+        }
+    }
+}
